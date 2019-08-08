7d859c92b9125f0a3688ea8b2b07adc3a65f59e3
Bloodrabbid <bloodrabbid@gmail.com>
2019-08-08 18:35:03 +1000

    add some function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,23 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main (String[] args) {
-		System.out.println ("Hey java");
+		
+		hey("Java");
+		double l = 5;
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь = " + area(l) );
+		System.out.println("Умножение = " + area(a, b));
+	}
+	public static void hey(String something) {
+		System.out.println("Hey " + something);
+	}
+	public static double area (double len) {
+		return len * len;
+	}
+
+	public static double area (double a, double b) {
+		return a + b;
+
 	}
 }
